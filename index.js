25682700ffc6e0e34f8839f61f473f72dda60596
Chris Bodley <chris.matthew.bodley@gmail.com>
2015-09-10 22:01:46 +0000

    just making the final commit will add to it once the 3d is in place

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ var app = express();
 var port = 3000;
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + '/public'));
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*
+keep this as an example of a get action
+app.get('/errg',function(req,res){
+  res.sendFile(__dirname +'/public/'+ "arrg.html");
+});
+*/
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -16,5 +27,13 @@ io.on('connection', function(client) {
 
     client.on('join', function(data) {
         console.log(data);
+    });
+    
+    client.on('user', function(data){
+        console.log(data);
+    });
+    
+    client.on('disconnect', function(){
+        console.log('user disconnected');
     });
 });
--- public/scripts/page.js
+++ public/scripts/page.js
@@ -1,7 +1,21 @@
- var socket = null
+ var socket = null;
+ var userName = null;
 $(document).ready(function() {
     socket = io();
     socket.on('connect', function(data) {
-        socket.emit('join', 'Hello World from client');
+        //socket.emit('join', 'Hello World from client');
+        
+        if (userName === null){
+            $('#myModal').modal('show');
+        }
     });
+    
+    
+    $('.btn-primary').on('click', function(d){
+        if($('#user').val() !== ""){
+            socket.emit('user', $('#user').val());
+             $('#myModal').modal('hide');
+        }
+    });
+    
 });
